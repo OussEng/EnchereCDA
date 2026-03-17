e60f229c493183f5240a8a6486de1f76400de3e4
Sckertch <mleda971@gmail.com>
2026-03-17 16:43:50 +0100

    Ajout des classes Categories manquantes

---
+++ src/main/java/fr/eni/enchere/categorie/bo/Categorie.java
@@ -0,0 +1,8 @@
+package fr.eni.enchere.categorie.bo;
+
+public class Categorie {
+    Long id;
+    String libelle;
+
+    public Categorie(){}
+}
---
+++ src/main/java/fr/eni/enchere/categorie/controller/CategorieController.java
@@ -0,0 +1,7 @@
+package fr.eni.enchere.categorie.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class CategorieController {
+}
