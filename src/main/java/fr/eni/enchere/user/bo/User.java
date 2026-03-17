db323f73d99090bba2e736dbf97e2e58d854fcdb
jnsualu2026 <nsualu.jonatan@gmail.com>
2026-03-17 16:02:20 +0100

    feat : add user module

---
+++ src/main/java/fr/eni/enchere/user/bo/User.java
@@ -0,0 +1,15 @@
+package fr.eni.enchere.user.bo;
+
+public class User {
+    private Long id;
+    private String pseudo;
+    private String prenom;
+    private String nom;
+    private String email;
+    private String telephone;
+    private String motDePasse;
+    private int credit;
+
+    public User() {
+    }
+}
