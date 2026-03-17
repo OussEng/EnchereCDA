c9719c3daaed9a6beaa48484b07b61d33d34684e
OussEng <oaitlahssaine@gmail.com>
2026-03-17 16:00:12 +0100

    creation article bo

--- src/main/java/fr/eni/enchere/article/bo/Article.java
+++ src/main/java/fr/eni/enchere/article/bo/Article.java
@@ -1,4 +1,17 @@
 package fr.eni.enchere.article.bo;
 
+import java.time.LocalDate;
+
 public class Article {
+
+    private Long id;
+    private String nom;
+    private String description;
+    private LocalDate dateDebutEncheres;
+    private LocalDate dateFinEncheres;
+    private int miseAPrix;
+    private int prixVente;
+
+    public Article() {
+    }
 }
