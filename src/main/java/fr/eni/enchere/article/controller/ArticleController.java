9eed3cd95c38deb4cc80df4f1235f96a4ec051db
jnsualu2026 <nsualu.jonatan@gmail.com>
2026-03-23 15:10:27 +0100

    maj branch

--- src/main/java/fr/eni/enchere/article/controller/ArticleController.java
+++ src/main/java/fr/eni/enchere/article/controller/ArticleController.java
@@ -11,7 +11,7 @@ import java.util.List;
 import java.util.Optional;
 
 @Controller
-@RequestMapping("/article")
+@RequestMapping("/articles")
 public class ArticleController {
 
     private final ArticleService articleService;
